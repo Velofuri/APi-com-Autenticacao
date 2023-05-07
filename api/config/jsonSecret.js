eba37dfcfa77c9836a84fa0796727261fd1a3bc8
Rafael <velofuri@gmail.com>
2023-05-07 16:07:53 -0300

    Aula 4

---
+++ api/config/jsonSecret.js
@@ -0,0 +1,3 @@
+module.exports = {
+  secret: "4d775d914c19002445f86c08f5eb8648"
+}
---
+++ api/routes/index.js
@@ -0,0 +1,15 @@
+const bodyParser = require('body-parser');
+
+const produto = require('./produtoRoute');
+const usuario = require('./usuariosRoutes');
+const auth = require('./authRoute')
+
+module.exports = (app) => {
+  app.use(
+    bodyParser.json(),
+    produto,
+    auth,
+    usuario,
+    );
+};
+
